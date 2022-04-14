2d7b4135b020257c2977ad68ef95bdfc5e37d18c
Lisin Alexandr <berserker37rus@gmail.com>
2022-04-14 15:23:49 +0300

    a

---
+++ js/action.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//       // инициализация слайдера
+//     const slider = new SimpleAdaptiveSlider('.slider', {
+//         autoplay: false,
+//         interval: 5000,
+//         swipe: true,
+//     });
+//     });
+
+    $(function() {
+
+      $('.rev-slider').slick()
+
+    })
